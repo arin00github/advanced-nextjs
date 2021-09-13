7d989f67cdee417d08be8a99a507457ec6673086
DESKTOP-3M1TBJ3\USER <leeleejin325@gmail.com>
2021-09-13 16:40:25 +0900

    update package

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.css';
+import App, { AppContext, AppInitialProps, AppProps } from 'next/app';
+import { Component } from 'react';
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Link from 'next/link';
+import axios from 'axios';
 
 export default function Home() {
-  return <div>Hello World</div>;
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  // onChange역할
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    console.log(event.target.files[0]);
+  };
+
+  // formData라는 instance에 담아 보냄
+  const handleFileUpload = () => {
+    const formData = new FormData();
+
+    formData.append('userfile', selectedFile, selectedFile.name);
+
+    axios
+      .post('api/uploadfile', formData)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div>
+      <div>file upload</div>
+      <div>
+        <input type="file" onChange={handleFileChange} />
+        <button onClick={handleFileUpload}>업로드</button>
+      </div>
+    </div>
+  );
 }
